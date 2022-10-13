e41dbe31903c94cb80286f45c172f5725db756e2
UgochukwuPeter <pjgraphixs@gmail.com>
2022-10-13 16:25:32 -0700

    updated

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include <unistd.h>
 
 /**
  * main - Prints quote to the standard error
@@ -8,6 +9,7 @@
 
 int main(void)
 {
-	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+	write (2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+	
 	return(1);
 }
