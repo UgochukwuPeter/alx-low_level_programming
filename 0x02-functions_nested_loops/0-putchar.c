8b34a41b7b96dd2409947c8134f983939b64999c
UgochukwuPeter <pjgraphixs@gmail.com>
2022-10-19 12:59:50 -0700

    initial commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - prints the string putchar
+ *
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	char *str;
+	int i;
+
+	str = "_putchar";
+	for(i= 0; i < 9; i++)
+	{
+	_putchar(str[i]);
+	}
+	_putchar("\n");
+	return (0);
+}
