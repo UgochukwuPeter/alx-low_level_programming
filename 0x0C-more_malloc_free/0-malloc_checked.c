2d87fb0a1f0af558f24a70bfd8b5b57c7d9e44b6
UgochukwuPeter <pjgraphixs@gmail.com>
2022-11-11 12:11:44 -0800

    initial commit

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+/**
+ * malloc_checked - a function that allocates memory use malloc
+ * @b: unsigned int type of variable
+ *
+ * Return: void
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+	if (ptr == NULL)
+		exit(98);
+	return (ptr);
+}
